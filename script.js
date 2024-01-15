836c4049b2a2796506b3ee51eaf9817b55ed914b
PuncharatLiu <156576344+PuncharatLiu@users.noreply.github.com>
2024-01-15 18:06:52 +0700

    Update script.js
    Added new var in main script to fix the error from Display.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // set default screen value to 0
 let displayScreen = 0;
 // set default two collect number to 0, 0
-let calculateNumber = [0, 0];
+let calculateNumber = ["0", "0"];
 // 
 let resultDisplay = false;
 // used to append text to screen
@@ -10,89 +10,111 @@ let displayElement = document.getElementById("display")
 let operatorSymbol = ["+", "-", "*", "/"];
 // used to collect the argument of updateDisplay() function
 let value;
-// ueed to store operator that will be calculated
+// used to store operator that will be calculated
 let toCalculate = "";
-// condition to check is it operator? 
+// condition to check if it's an operator
 let isSymbol = false;
-//get the element that will be change the color when user click on the button.
+// get the element that will be changed in color when user clicks on the button.
 let ButtonToChangeColor;
+// remember the symbol of the last clicked operator
+let rememberSymbol = "";
 
-
-// This function used to append the text to screen and display the change color on the button that user click.
-function updateDisplay(getValue){
+// append the text to the screen and display the changed color on the button that the user clicks.
+function updateDisplay(getValue) {
     // copy the value. 
     value = getValue;
-    
-    // If there's no number append to screen
-    if (displayScreen === 0 || resultDisplay){
-        // Call the checkOperatorSymbol();
-        checkOperatorSymbol();
+
+    // If there's no number appended to the screen
+    if (displayScreen === 0 || resultDisplay) {
+        checkOperatorSymbol();  // Call the checkOperatorSymbol();
         if (isSymbol) {
-            // highlight the operator button that user click
-            changeOperatorButtonColor();
+            
+            changeOperatorButtonColor(); // highlight the operator button that user clicked
+            
+            if (rememberSymbol !== "") {
+                removePrevious();
+            }
         } else {
-            // add value get when user click 
-            displayScreen = getValue;
+            displayScreen = getValue; // add value gotten when user clicks 
         }
 
     } else {
+        
         checkOperatorSymbol();
+        
         if (isSymbol) {
             changeOperatorButtonColor();
         } else {
-            displayScreen += getValue; 
+            displayScreen += getValue;
         }
+
     };
 
     resultDisplay = false;
     showOnDisplay();
 }
 
-
-//
-function showOnDisplay(){
+// Function to display the content on the screen
+function showOnDisplay() {
     displayElement.textContent = displayScreen;
 }
 
-
-/*
-    1. Check the botton that user click is number or operator. 
-*/
-function checkOperatorSymbol(){
-    // Check the botton that user click is operator. and return boolean then store in "isOperatorInList" 
+// Check if the button that the user clicked is a number or an operator
+function checkOperatorSymbol() {
+    // Check if the button that the user clicked is an operator and return boolean then store in "isOperatorInList" 
     let isOperatorInList = operatorSymbol.includes(value);
-    
-    // If user click operator button
-    if(isOperatorInList){
-        // Store the number that user enter when user click operator button 
+    let i = 0;
+    // If the user clicked the operator button
+    if (isOperatorInList) {
+        // Store the number that the user entered when the user clicked the operator button 
         calculateNumber[i] = displayScreen;
-        // Used in if conditon in updateDisplay() function to update screen
+        // Used in if condition in the updateDisplay() function to update the screen
         isSymbol = true;
-        // Used to calculate when the user click "=" 
+        // Used to calculate when the user clicks "=" 
         toCalculate = value;
     }
 }
 
-// Change the color of operator that user click.
-function changeOperatorButtonColor(){
+// Change the color of the operator that the user clicked.
+function changeOperatorButtonColor() {
+    resetPrevious();
     getButtonTochangeColor();
     ButtonToChangeColor.classList.add("clicked");
 }
 
-function getButtonTochangeColor(){
-    
+// Reset the color of the previously clicked operator button
+function resetPrevious() {
+    if (rememberSymbol !== "") {
+        let previousButton = document.getElementById(rememberSymbol);
+        previousButton.classList.remove("clicked");
+        previousButton.classList.add("operator-button");
+    }
+}
+
+// Function to remove the "clicked" class and add back the "operator-button" class
+function removePrevious() {
+    ButtonToChangeColor.classList.remove("clicked");
+    ButtonToChangeColor.classList.add("operator-button");
+}
+
+// Get the button to change color based on the operator
+function getButtonTochangeColor() {
     switch (toCalculate) {
         case "/":
             ButtonToChangeColor = document.getElementById("divide");
+            rememberSymbol = "divide"
             break;
         case "*":
             ButtonToChangeColor = document.getElementById("multiply");
+            rememberSymbol = "multiply"
             break;
         case "+":
             ButtonToChangeColor = document.getElementById("plus");
+            rememberSymbol = "plus"
             break;
         case "-":
             ButtonToChangeColor = document.getElementById("minus");
+            rememberSymbol = "minus"
             break;
     }
 }
