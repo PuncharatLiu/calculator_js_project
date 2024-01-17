f13141f727874236a052ee17a8411c27061915eb
PuncharatLiu <156576344+PuncharatLiu@users.noreply.github.com>
2024-01-17 23:43:55 +0700

    History and another screen feature

--- script.js
+++ script.js
@@ -1,32 +1,104 @@
+let hold = "0";
 let display = "0";
+let displayBackUp = "0";
+let firstClick = true;
+let screenHistory = document.getElementById("history");
 let screen = document.getElementById("display");
+let voidValue = ""
+let operator = ["+", "-", "*", "/"];
+let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 function updateDisplay(value){
-	if (display === "0"){
+	voidValue = value;
+	removeNumAfterOperator();
+	if (display === "0" && number.includes(voidValue)){
 		display = value;
+		firstAssighSecondPush();
 	} else {
-		display += value;
+		hideOperator();
 	}
-	screen.innerText = display;
+	
+	switchDisplay();
+	
+	
+	console.log(hold, display);
+	console.log(operator.includes(voidValue));
+
+
 }
 
 function calculate(){
-	try {
-		let result = eval(display);
-		screen.innerText = result;
-		display = result;
-	} catch (error) {
+	 try {
+	 
+	 	let result = eval(hold);
+	 	screen.innerText = result;
+	 	displayHistory();
+	 	display = result.toString();
+	 } catch (error) {
 		screen.innerText = "Error";
 	}
+
 } 
+
+function displayHistory(){
+	screenHistory.textContent = hold;
+}
 
 function allClear(){
 	display = "0";
+	hold = "0";
+	firstClick = true;
+
 	screen.textContent = display;
-	return false;
+	screenHistory.textContent = hold;
 }
 
 function deleteNumber(){
-	display = display.slice(0, -1);
-	screen.textContent = display;
+	let delateLastNumber =  hold.slice(0, -1);
+	hold = delateLastNumber;
+	screen.textContent = hold;
+}
+
+function calculatePrecent(){
+	let precent = hold/100;
+	screen.textContent = precent;
+}
+
+function hideOperator(){
+	let isOperator = operator.includes(voidValue);
+	
+	if (isOperator) {
+		hold += voidValue;
+	} else {
+		display += voidValue;
+		hold += display[1];
+	}
+}
+
+function removeNumAfterOperator(){
+	let isOperator = operator.includes(voidValue);
+	if (isOperator) {
+		displayBackUp = display;
+		display = "0";
+	}
+	// screen.textContent = displayBackUp
+}
+
+function switchDisplay (){
+	let isOperator = operator.includes(voidValue);
+	if (isOperator) {
+		screen.textContent = displayBackUp;
+		console.log("yes")
+	} else {
+		screen.textContent = display;
+	}
+}
+
+function firstAssighSecondPush (){
+	if (firstClick){
+		hold = voidValue;
+		firstClick = false;
+	} else {
+		hold += voidValue;
+	}
 }
